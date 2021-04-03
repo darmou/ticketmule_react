783e461db51d048bb0bdb1d581e6033456a43397
darmou <dmoulder@qtome.com>
2021-04-03 13:48:30 -0700

    fix issues with broken config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -8,15 +8,15 @@ const path = require("path");
 environment.plugins.append(
     "ForkTsCheckerWebpackPlugin",
     new ForkTsCheckerWebpackPlugin({
-        typescript: {
-            configFile: path.resolve(__dirname, "../../tsconfig.json"),
-        },
-        async: false,
+      typescript: {
+        configFile: path.resolve(__dirname, "../../tsconfig.json"),
+      },
+      async: false,
     })
 );
 
 if (!module.hot) {
-    environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.sourceMapContents = false;
+  environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.sourceMapContents = false;
 }
 
 module.exports = environment.toWebpackConfig();
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,6 +7,6 @@ module.exports = environment
 const nodeModulesLoader = environment.loaders.get('nodeModules')
 
 if (!Array.isArray(nodeModulesLoader.exclude)) {
-    nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null) ? [] : [nodeModulesLoader.exclude]
+  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null) ? [] : [nodeModulesLoader.exclude]
 }
 nodeModulesLoader.exclude.push(/react-table/)
