4fb5f0a34807d990eb16ed49df7637855c24d004
Daryl Moulder <dmoulder@qtome.com>
2022-01-18 22:15:48 -0800

    upgrade usage for new packages

--- app/javascript/components/Contacts/ContactForm.tsx
+++ app/javascript/components/Contacts/ContactForm.tsx
@@ -22,33 +22,33 @@ const ContactForm = ({formAction, slug, contact}) => {
     return(<StyledForm acceptCharset="UTF-8" onSubmit={handleSubmit(onSubmit.bind(this))}>
         <Row>
             <StyledLabel>First name:</StyledLabel>
-            <StyledInput name="first_name" ref={register} defaultValue={(contact) ? contact.first_name : ""} type="text"/>
+            <StyledInput name="first_name" {...register("first_name")} defaultValue={(contact) ? contact.first_name : ""} type="text"/>
         </Row>
         <Row>
             <StyledLabel>Last name:</StyledLabel>
-            <StyledInput name="last_name" ref={register} defaultValue={(contact) ? contact.last_name : ""} type="text"/>
+            <StyledInput name="last_name" {...register("last_name")} defaultValue={(contact) ? contact.last_name : ""} type="text"/>
         </Row>
         <Row>
             <StyledLabel>Email:</StyledLabel>
-            <StyledInput name="email" ref={register} defaultValue={(contact) ? contact.email : ""} type="text"/>
+            <StyledInput name="email" {...register("email")} defaultValue={(contact) ? contact.email : ""} type="text"/>
         </Row>
         <Row>
             <StyledLabel>Mobile phone:</StyledLabel>
-            <StyledInput name="mobile_phone" ref={register} defaultValue={(contact) ? contact.mobile_phone : ""} type="text"/>
+            <StyledInput name="mobile_phone" {...register("mobile_phone")} defaultValue={(contact) ? contact.mobile_phone : ""} type="text"/>
         </Row>
         <Row>
             <StyledLabel>Office phone:</StyledLabel>
-            <StyledInput name="office_phone" ref={register} defaultValue={(contact) ? contact.office_phone : ""} type="text"/>
+            <StyledInput name="office_phone" {...register("office_phone")} defaultValue={(contact) ? contact.office_phone : ""} type="text"/>
         </Row>
 
         <Row>
             <StyledLabel>Affiliation:</StyledLabel>
-            <StyledInput name="affiliation" ref={register} defaultValue={(contact) ? contact.affiliation : ""} type="text"/>
+            <StyledInput name="affiliation" {...register("affiliation")} defaultValue={(contact) ? contact.affiliation : ""} type="text"/>
         </Row>
 
         <Row>
             <StyledLabel>Notes: </StyledLabel>
-            <PaddedTextarea defaultValue={(contact) ? contact.notes : ""} name="notes" ref={register} cols="45" rows="6"/>
+            <PaddedTextarea defaultValue={(contact) ? contact.notes : ""} name="notes" {...register("notes")} cols="45" rows="6"/>
         </Row>
         <Row>
             <StyledLabel>&nbsp;</StyledLabel>
