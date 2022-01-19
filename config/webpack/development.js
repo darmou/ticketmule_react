4aea95943e4cd661aeb51eb9dc8d53bd6919706c
Daryl Moulder <dmoulder@qtome.com>
2022-01-18 16:12:57 -0800

    upgrade webpacker

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,21 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const environment = require('./environment')
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const path = require("path");
+const webpackConfig = require('./base')
 
-environment.plugins.append(
-    "ForkTsCheckerWebpackPlugin",
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        configFile: path.resolve(__dirname, "../../tsconfig.json"),
-      },
-      async: false,
-    })
-);
-
-if (!module.hot) {
-  environment.loaders.get('sass').use.find(item => item.loader === 'sass-loader').options.sourceMapContents = false;
-}
-
-module.exports = environment.toWebpackConfig();
+module.exports = webpackConfig
